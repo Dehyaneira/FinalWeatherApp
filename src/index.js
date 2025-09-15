982520d226c04e17cbefdf71ea0d6e82704e4b98
Dehyaneira Guevara <dehyaneira24@gmail.com>
2025-09-15 13:35:16 -0500

    Add the days to the forecast

--- src/index.js
+++ src/index.js
@@ -52,7 +52,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+    <div class="weather-app-forecast-day">
+            <div class="weather-app-forecast-date">${day}</div>
+            <div class="weather-app-forecast-icon">⛅</div>
+            <div class="weather-app-forecast-temperatures">
+              <div class="weather-app-forecast-temperature">
+                <strong>15°</strong>
+              </div>
+              <div class="weather-app-forecast-temperature">9°</div>
+            </div>
+          </div>
+          `;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Quito");
+displayForecast();
